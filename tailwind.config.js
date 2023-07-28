65b5c5d424078a8402168834f7838c54a9b99667
multiii <72256091+multiii@users.noreply.github.com>
2023-07-29 03:51:56 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./{html,js}"],
+    prefix: 'tw-',
+    theme: {
+        fontSize: {
+            'hxl': ['1.025rem', '1.65rem']
+        }
+    }
+}
